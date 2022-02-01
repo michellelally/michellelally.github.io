1b5e75a99f212d142cbbe61c525ed3b4d168e50c
michellelally <shelley.lally@gmail.com>
2022-02-01 17:55:41 +0000

    updated menu on portfolio

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,25 @@
 
 function openNav() {
-    document.getElementById("menu").style.width = "100%";
+  document.getElementById("menu").style.width = "100%";
+  let marquee = document.getElementsByClassName("marquee");
+
+  for (let i = 0; i <= marquee.length; i++) {
+    if (marquee[i] != null) {
+      marquee[i].style.visibility = 'hidden';
+    }
   }
-  
-  function closeNav() {
-    document.getElementById("menu").style.width = "0%";
+}
+
+function closeNav() {
+  document.getElementById("menu").style.width = "0%";
+  let marquee = document.getElementsByClassName("marquee");
+
+  for (let i = 0; i <= marquee.length; i++){
+    if (marquee[i] != null){
+      marquee[i].style.visibility = 'visible';
+    }
   }
+
+
+
+}
