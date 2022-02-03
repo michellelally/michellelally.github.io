293e26f3b81813f80b346e76020c6216d7988eb5
michellelally <shelley.lally@gmail.com>
2022-02-03 15:46:56 +0000

    fixed menu, js visitor counter

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,15 @@
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/michellally.github.io/554512c9-5705-4321-9505-7635abd8c47f/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		console.log(res.value);
+	})
+}
+
 window.requestAnimFrame = (function () {
   return window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
